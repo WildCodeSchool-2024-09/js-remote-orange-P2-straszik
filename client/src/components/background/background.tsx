b7ea158cd03d01dab76eda91f5f17e716812c694
MikeHatchi22 <hatchi.mike@gmail.com>
2024-11-12 17:35:33 +0100

    debut album

--- client/src/components/background/background.tsx
+++ client/src/components/background/background.tsx
@@ -1,17 +1,83 @@
+import React, { useState, useEffect } from "react";
 import "./Background.css";
 
+const getRandomVideo = () => {
+    const videos = [
+        { id: "BtyHYIpykN0", start: 0 },
+        { id: "IOwom_Gp__Q", start: 15 },
+        { id: "YdjO4EpEzZw", start: 15 },
+        { id: "CEbJwZOkvDM", start: 20 }
+    ];
+    const randomIndex = Math.floor(Math.random() * videos.length);
+    return videos[randomIndex];
+};
+
 const BackgroundVideo = () => {
+    const [isMuted, setIsMuted] = useState(true);
+    const [videoId, setVideoId] = useState<string>(getRandomVideo().id);
+    const [startTime, setStartTime] = useState<number>(getRandomVideo().start);
+    const [player, setPlayer] = useState<any>(null);
+
+    useEffect(() => {
+        const onYouTubeIframeAPIReady = () => {
+            const newPlayer = new window.YT.Player("youtube-player", {
+                videoId: videoId,
+                playerVars: {
+                    start: startTime,  // Début de la vidéo à partir de la seconde spécifiée
+                },
+                events: {
+                    onReady: (event: any) => {
+                        const youtubePlayer = event.target;
+                        setPlayer(youtubePlayer);
+                        youtubePlayer.playVideo();
+                        if (isMuted) {
+                            youtubePlayer.mute();
+                        } else {
+                            youtubePlayer.unMute();
+                        }
+                    }
+                }
+            });
+        };
+
+        if (!window.YT) {
+            const tag = document.createElement("script");
+            tag.src = "https://www.youtube.com/iframe_api";
+            const firstScriptTag = document.getElementsByTagName("script")[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+            window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
+        } else {
+            onYouTubeIframeAPIReady();
+        }
+    }, [videoId, startTime]);
+
+    const toggleMute = () => {
+        if (player) {
+            if (isMuted) {
+                player.unMute();
+            } else {
+                player.mute();
+            }
+            setIsMuted(!isMuted);
+        }
+    };
+
+    const changeVideo = () => {
+        const randomVideo = getRandomVideo();
+        setVideoId(randomVideo.id);
+        setStartTime(randomVideo.start);
+    };
+
     return (
         <div className="video-container">
-            <iframe
-                className="background-video"
-                src="https://www.youtube.com/embed/BtyHYIpykN0?autoplay=1&mute=1&loop=1&playlist=BtyHYIpykN0&controls=0&showinfo=0&modestbranding=1"
-                title="YouTube Background Video"
-                frameBorder="0"
-                allow="autoplay; fullscreen"
-                allowFullScreen
-            />
-            <div className="content">{/* Add foreground content here */}</div>
+            <div id="youtube-player" className="background-video"></div>
+            <button className="mute-button" onClick={toggleMute}>
+                {isMuted ? "🔇" : "🔊"}
+            </button>
+            <button className="change-video-button" onClick={changeVideo}>
+                Changer la vidéo
+            </button>
+            <div className="content"></div>
         </div>
     );
 };
--- client/src/pages/Album/Album.tsx
+++ client/src/pages/Album/Album.tsx
@@ -7,42 +7,40 @@ function Albums() {
     const albumCovers = [
         {
             id: 1,
-            src: '/path/to/album1.png',
-            title: "Album 1"
+            src: 'https://m.media-amazon.com/images/I/51aNO1PUtfL._UXNaN_FMjpg_QL85_.jpg',
+            title: "Que La Famille"
         },
-
         {
             id: 2,
-            src: '/path/to/album2.png',
-            title: "Album 2"
+            src: 'https://cdns-images.dzcdn.net/images/cover/3c0d34f7576d81c8f269570d9806fb88/500x500.jpg',
+            title: "Le Monde Chico"
         },
-
         {
             id: 3,
-            src: '/path/to/album3.png',
-            title: "Album 3"
+            src: 'https://m.media-amazon.com/images/I/71dFTmV2jgL._UF1000,1000_QL80_.jpg',
+            title: "Dans la legende "
         },
-
         {
             id: 4,
-            src: '/path/to/album4.png',
-            title: "Album 4"
+            src: 'https://i.scdn.co/image/ab67616d0000b2736c3966c4dd0eb2273696fe16',
+            title: "Deux fréres"
         },
-
     ];
+
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const nextSlide = () => {
+
         setCurrentIndex((prevIndex) => (prevIndex + 1) % albumCovers.length);
     };
 
     const prevSlide = () => {
+
         setCurrentIndex((prevIndex) => (prevIndex - 1 + albumCovers.length) % albumCovers.length);
     };
 
     return (
         <div className="albums-container">
-
             <div className="carousel">
                 <button onClick={prevSlide} className="carousel-button prev">
                     &lt;
