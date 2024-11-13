6f0fe35ac6e494cee0de663fd7db495fe1b07e76
MikeHatchi22 <hatchi.mike@gmail.com>
2024-11-13 17:26:45 +0100

    fix background album

--- client/src/pages/AlbumItem/AlbumItem.tsx
+++ client/src/pages/AlbumItem/AlbumItem.tsx
@@ -1,22 +1,95 @@
 import { useParams } from "react-router-dom";
+import './AlbumItem.css';
+import AlbumList from "../../components/AlbumList/AlbumList";
+
+const albums = [
+    {
+        id: 1,
+        title: "Album PNL Test",
+        imgSrc: "",
+        description: "Description de l'album 1",
+        song: [
+            {
+                id: 1,
+                title: "Song 1",
+                description: "Description de la chanson 1"
+            },
+            {
+                id: 2,
+                title: "Song 2",
+                description: "Description de la chanson 2"
+            },
+            {
+                id: 3,
+                title: "Song 3",
+                description: "Description de la chanson 3"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Album 2",
+        description: "Description de l'album 2",
+        song: [
+            {
+                id: 1,
+                title: "Song 1",
+                description: "Description de la chanson 1"
+            },
+            {
+                id: 2,
+                title: "Song 2",
+                description: "Description de la chanson 2"
+            },
+            {
+                id: 3,
+                title: "Song 3",
+                description: "Description de la chanson 3"
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Album 3",
+        description: "Description de l'album 3",
+        song: [
+            {
+                id: 1,
+                title: "Song 1",
+                description: "Description de la chanson 1"
+            },
+            {
+                id: 2,
+                title: "Song 2",
+                description: "Description de la chanson 2"
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Album 4",
+        description: "Description de l'album 4",
+        song: [
+            {
+                id: 1,
+                title: "Song 1",
+                description: "Description de la chanson 1"
+            },
+            {
+                id: 2,
+                title: "Song 2",
+                description: "Description de la chanson 2"
+            }
+        ]
+    }
+]
 
 function AlbumItem() {
     const { id } = useParams();
     return (
-        <main className="main">
-            <section className="hero">
-                <h1>Album Title {id}</h1>
-                <p>Album Description</p>
-            </section>
-            <section className="content">
-                <div className="item-rows">
-                    <div className="song-title">
-                        <p>01</p>
-                        <h3>Song Title</h3>
-                    </div>
-                </div>
-            </section>
-        </main>
+        <div className="backgroundalbum">
+            <AlbumList albums={albums.map(album => ({ ...album, song: album.song.map(song => song.title) }))} />
+        </div>
     );
 }
 
