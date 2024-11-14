6393e85384c4ed21a550dccbc7a04e586eabfa91
SGriscelli <Simon.griscelli@wanadoo.fr>
2024-11-14 22:13:16 +0100

    better control button

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -8,16 +8,37 @@ const Footer: React.FC = () => {
 
       <footer className="footer">
         <div className="social-icons">
-          <a href="https://www.youtube.com/channel/UCtBjt766LG9EDVKZx4Q4IHw" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.youtube.com/channel/UCtBjt766LG9EDVKZx4Q4IHw"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="YouTube"
+          >
             <img src="/assets/images/icon-youtube.svg" alt="YouTube" />
           </a>
-          <a href="https://www.facebook.com/Pnlmusic/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.facebook.com/Pnlmusic/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Facebook"
+          >
             <img src="/assets/images/icon-facebook.svg" alt="Facebook" />
           </a>
-          <a href="https://www.instagram.com/pnlofficiel/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.instagram.com/pnlofficiel/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
+          >
             <img src="/assets/images/icon-instagram.svg" alt="Instagram" />
           </a>
-          <a href="https://twitter.com/pnlofficiel" target="_blank" rel="noopener noreferrer" className="st-icon">
+          <a
+            href="https://twitter.com/pnlofficiel"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="st-icon"
+            aria-label="Twitter"
+          >
             <img src="/assets/images/icon-twitter.svg" alt="Twitter" />
           </a>
         </div>
