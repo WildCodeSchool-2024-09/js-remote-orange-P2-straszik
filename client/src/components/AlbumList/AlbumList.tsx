340cd885b35d431f32a4eb88c2e27b618594190a
MikeHatchi22 <hatchi.mike@gmail.com>
2024-11-18 14:16:58 +0100

    Create AlbumList.css & AlbumListItem.css

--- client/src/components/AlbumList/AlbumList.tsx
+++ client/src/components/AlbumList/AlbumList.tsx
@@ -1,15 +1,17 @@
 import AlbumListItem from "./AlbumListItem";
+import "./AlbumList.css";
 
 type Album = {
   id: number;
   title: string;
+  imgSrc: string;
   description: string;
   song: string[];
 };
 
 function AlbumList({ albums }: { albums: Album[] }) {
   return (
-    <div>
+    <div className="album-list">
       <h1>Album List</h1>
       {albums?.map((album: Album) => (
         <AlbumListItem album={album} key={album.id} />
--- client/src/components/AlbumList/AlbumListItem.tsx
+++ client/src/components/AlbumList/AlbumListItem.tsx
@@ -1,15 +1,19 @@
+import "./AlbumListItem.css";
+
 type Album = {
   id: number;
   title: string;
+  imgSrc: string;
   description: string;
   song: string[];
 };
 
 function AlbumListItem({ album }: { album: Album }) {
   return (
-    <div>
-      <h2>{album.title}</h2>
-      <p>{album.description}</p>
+    <div className="album-list-item">
+      <img src={album.imgSrc} alt={album.title} />
+      <h2 className="album-title">{album.title}</h2>
+      <p className="album-description">{album.description}</p>
     </div>
   );
 }
