bfd853fbf740879f6212e4a8529d70bed3eb5cce
SGriscelli <Simon.griscelli@wanadoo.fr>
2024-11-26 18:02:36 +0100

    fix du menu burger (lienà

--- client/src/components/BurgerMenu/BurgerMenu.tsx
+++ client/src/components/BurgerMenu/BurgerMenu.tsx
@@ -37,7 +37,7 @@ const BurgerMenu = () => {
             </Link>
           </li>
           <li>
-            <Link to="#">
+            <Link to="/concerts">
               <strong>Concerts</strong>
             </Link>
           </li>
